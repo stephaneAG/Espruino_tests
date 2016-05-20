6caf6dc04bfd9722c48a9ccfc7941de83479a78f
Stéphane Adam Garnier <seedsodesign@gmail.com>
2016-05-20 19:25:38 +0200

    Update SoftPotReads_wip.js

--- SoftPotTest/SoftPotReads_wip.js
+++ SoftPotTest/SoftPotReads_wip.js
@@ -36,3 +36,10 @@ checkTouch_timeout = setTimeout(function checkTouch(){
 
 
 // differentiate touchstart from touchend
+// when touched, register the smallest steps ( while averaging if possible )
+// to detect the direction, we check if the curr voltage is bigger or smaller than the previous one
+// to detect the speed, we check the difference between the current voltage & the previous one:
+//   depending on how many "smallest steps" it can be divided into, we alter the playback rate by some corresponding amount
+// Nbs:
+// - the "smallest steps" could/should be configurable
+// - the speed <-> playbackrate mapping could/should be mappable in its sensibility ( "how far", as smallestSteps/timeBetweenTwoReads, represents "how much")
